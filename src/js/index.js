cb69e884a178f30248b7783dba43dc05681e3393
Joanna Makarewicz <joanna.milun@gmail.com>
2023-01-25 17:38:20 +0100

    hamburger menu added

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,9 @@ import '../scss/main.scss';
 
 
 console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!')
+
+const hamburger = document.querySelector('.header__hamburger--js');
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.nav--js');
+    nav.classList.toggle('nav__open')
+})
